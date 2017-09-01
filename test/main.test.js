29f6d340d1c3a5b8c2cdd90b33b9f43de3456103
Greg Borota <borota@gmail.com>
2017-09-01 14:35:28 -0500

    Unit test stub methods

---
+++ test/main.test.js
@@ -0,0 +1,93 @@
+const mapper = require('../build/sedra-cal.js');
+
+/**
+ * General case usage, with one-to-one mapping
+ * @param {object} test 
+ */
+const sedra2cal_generic = (test) => {
+  test.expect(1);
+  test.ok(true, 'sedra2cal_generic pass');
+  test.done();
+};
+
+/**
+ * Word with (i;) => (yi) mapping
+ * @param {object} test 
+ */
+const sedra2cal_yi = (test) => {
+  test.expect(1);
+  test.ok(true, 'sedra2cal_yi pass');
+  test.done();
+};
+
+/**
+ * Word with (uO) => (wu) mapping
+ * @param {object} test 
+ */
+const sedra2cal_wu = (test) => {
+  test.expect(1);
+  test.ok(true, 'sedra2cal pass');
+  test.done();
+};
+
+/**
+ * Word with (oO) => (wO) mapping
+ * @param {object} test 
+ */
+const sedra2cal_wO = (test) => {
+  test.expect(1);
+  test.ok(true, 'cal2sedra_wO pass');
+  test.done();
+};
+
+/**
+ * General case cal2sedra usage, with one-to-one mapping
+ * @param {object} test 
+ */
+const cal2sedra_generic = (test) => {
+  test.expect(1);
+  test.ok(true, 'cal2sedra_generic pass');
+  test.done();
+};
+
+/**
+ * Word with (yi) => (i;) mapping
+ * @param {object} test 
+ */
+const cal2sedra_yi = (test) => {
+  test.expect(1);
+  test.ok(true, 'cal2sedra_generic pass');
+  test.done();
+};
+
+/**
+ * Word with (wu) => (uO) mapping
+ * @param {object} test 
+ */
+const cal2sedra_wu = (test) => {
+  test.expect(1);
+  test.ok(true, 'cal2sedra_wu pass');
+  test.done();
+};
+
+/**
+ * Word with (wO) => (oO) mapping
+ * @param {object} test 
+ */
+const cal2sedra_wO = (test) => {
+  test.expect(1);
+  test.ok(true, 'cal2sedra_wO pass');
+  test.done();
+};
+
+module.exports = {
+  sedra2cal_generic,
+  sedra2cal_yi,
+  sedra2cal_wu,
+  sedra2cal_wO,
+
+  cal2sedra_generic,
+  cal2sedra_yi,
+  cal2sedra_wu,
+  cal2sedra_wO
+};
